b0297e069394b9ee68554424f724cbee3af2d7d8
Fernando Alves <f.alves@reply.com>
2020-10-27 15:09:43 -0300

    Init 🥳 projeto Add FluentValidation

---
+++ Medium.Validation/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medium.Validation.Controllers
+{
+    /// <summary>
+    /// Customers resource
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        /// <summary>
+        /// Create Customer
+        /// </summary>
+        [HttpPost()]
+        public ActionResult Post([FromBody]CustomerPost customerPost)
+        {
+            // Criação de Customer em service.
+
+            return Created(string.Empty, 123);
+        }
+    }
+}
---
+++ Medium.Validation/Models/Requests/CustomerPost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FluentValidation;
+
+namespace Medium.Validation
+{
+    public class CustomerPost
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Gender { get; set; }
+        public bool Draft { get; set; }
+    }
+
+    public class CustomerPostValidator : AbstractValidator<CustomerPost>
+    {
+        public CustomerPostValidator()
+        {
+            RuleFor(customer => customer.Name).NotEmpty();
+
+            When(customer => !customer.Draft, () => {
+                RuleFor(customer => customer.Birthday).NotEmpty();
+                RuleFor(customer => customer.Email).NotEmpty();
+                RuleFor(customer => customer.PhoneNumber).NotEmpty();
+                RuleFor(customer => customer.Gender).NotEmpty();
+            });
+        }            
+    }
+}
